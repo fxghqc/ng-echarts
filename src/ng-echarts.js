380942f348861ba0922b747df1a417e075aedabd
fxghqc <konxiao@gmail.com>
2015-07-04 14:38:53 +0800

    remove unnecessary width and add echart service

--- src/ng-echarts.js
+++ src/ng-echarts.js
@@ -10,8 +10,12 @@
     };
 
     function calSize(ele, chart) {
-      width  = ele.style.width  || chart.offsetWidth || '500px';
-      height = ele.style.height || ele.offsetHeight  || '300px';
+      width  = ele.style.width;
+      height = ele.style.height || ele.offsetHeight || '300px';
+
+      if ((height + '').indexOf('px') < 0) { height = height + 'px'; }
+      // this is not necessary
+      //if ((width + '').indexOf('px') < 0) { width = width + 'px'; }
 
       return { width: width, height: height };
     };
@@ -56,6 +60,89 @@
       template: '<div></div>',
       restrict: 'E',
       link: link
+    };
+  }])
+
+  .factory('echart', ['$window', '$log', function($window, $log) {
+    function range(n) {
+      var i = -1;
+      var a = [];
+
+      while (++i < n) {
+        a[i] = i;
+      }
+
+      return a;
+    }
+    function generateXdata(n) {
+      return range(n);
+    }
+
+    function generateYdata(n) {
+      return range(n).map(function() {
+        return $window.Math.round($window.Math.random() * 100);
+      });
+    }
+
+    function generateSeriess(n, line) {
+      line = line || 1;
+      return range(line).map(function(i) {
+        return generateSeries(i, 'line', n);
+      });
+    }
+
+    function generateSeries(title, type, n) {
+      return {
+        name: title,
+        type: type,
+        //showAllSymbol: true,
+        //symbolSize: function (value) {
+        //  return Math.round(value[2] / 10) + 2;
+        //},
+        data: generateYdata(n)
+      };
+    }
+
+    function getSampleChart() {
+      var n = 20;
+      return {
+        option: {
+          title: {
+            text: 'sample',
+            textStyle: { color: '#505050' }
+          },
+          backgroundColor: { color: 'rgba(0,0,0,0)'},
+          theme: 'macarons',
+          tooltip: {
+            trigger: 'item',
+            formatter: function (params) {
+              return params.seriesName + '<br>' + params.name.toLocaleString() + ': ' + params.value[1];
+            }
+          },
+          legend: { data: []},
+          xAxis: [{
+            type: 'category',
+            axisLine: { lineStyle: { color: '#b3bfc3', width: 1 } },
+            axisTick: { lineStyle: { color: '#b3bfc3', width: 1 } },
+            axisLabel: { textStyle: { color: '#5c5c5c' } },
+            data: generateXdata(n)
+          }],
+          yAxis: [{
+            type: 'value',
+            axisLine: { lineStyle: { color: '#b3bfc3', width: 1 } },
+            axisTick: { lineStyle: { color: '#b3bfc3', width: 1 } },
+            axisLabel: { textStyle: { color: '#5c5c5c' } }
+          }],
+          series: generateSeriess(n, 1)
+        },
+        theme: 'macarons',
+        data: null,
+        autoResize: true
+      };
+    }
+
+    return {
+      getSampleChart: getSampleChart
     };
   }]);
 })();
